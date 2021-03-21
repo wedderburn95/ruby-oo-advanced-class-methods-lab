dbb31b4ccf5fdbb6a271d6c78b310821495d9e12
william <wedderburn95@yahoo.com>
2021-03-21 19:27:57 -0400

    commit tree e0b612bd2456de30842bdac290c718fac923db19

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,76 @@
 class Song
   attr_accessor :name, :artist_name
-  @@all = []
+  @@all = [] #Class variable thats an empty array
 
   def self.all
     @@all
   end
 
   def save
-    self.class.all << self
+    self.class.all << self #adding on to the array on line #3
   end
+
+  def self.create
+    song = self.new # created an local variable to store an newly instanciated song
+    song.save.last #instance maethod saved. invoked on the instance. from the array returned retrieve the last instance
+  end
+
+  def self.new_by_name(name)
+    song = self.new #created an local variable to store an newly instanciated song
+    song.name = name #retrieved the instance stored inside the local variable invoked the setter name=(setter). called the one of the four methods created by line two
+    song
+  end
+
+  def self.create_by_name(name) #calling the name of the song
+    song =self.new
+    song.name = name 
+    song.save #Calling the save method
+    return song
+  end
+
+  def self.artist_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect{|s| s.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|s| s.name}
+  end
+
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+  
 
 end
